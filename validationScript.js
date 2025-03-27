151ea04e83eb3a8906f43d67a2011578762a5ae4
Bianca Baccay <bzbaccay@my.waketech.edu>
2025-03-27 12:53:40 -0400

    finished

--- validationScript.js
+++ validationScript.js
@@ -10,3 +10,21 @@
         // Valid input: display confirmation and submit the form
 
         // Invalid input: display error message
+
+let inputField; // input variable
+let submit = document.getElementById("submit"); // submit button
+
+submit.addEventListener("click", checkInput);
+
+function checkInput() {
+  const checkAlphaNum = /^[a-zA-Z0-9]+$/; // used to compare if input is alphanumeric
+  inputField = document.getElementById("inputField")
+
+  if (checkAlphaNum.test(inputField.value)) {
+    window.alert("Input is the correct format and has been submitted.")
+    inputField.setCustomValidity("")
+  } else {
+    inputField.setCustomValidity("Error, input isn't alpanumeric.")
+  }
+}
+
